4bc3748fdac84238664e4558fe5d9a7dc5b9dc56
chenyg5 <chenyg5@asiainfo.com>
2021-02-01 11:14:50 +0800

    类加载器

---
+++ src/main/java/com/cyg/study/jvm/ClassLoaderDemo.java
@@ -0,0 +1,16 @@
+package com.cyg.study.jvm;
+
+/**
+ * @author cyg
+ * @date 2021/2/1 11:12
+ **/
+public class ClassLoaderDemo {
+    /**
+     * 每个类加载都有一个父类加载器，我们通过下面的程序来验证。
+     **/
+    public static void main(String[] args) {
+        System.out.println("ClassLodarDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader());
+        System.out.println("The Parent of ClassLodarDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader().getParent());
+        System.out.println("The GrandParent of ClassLodarDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader().getParent().getParent());
+    }
+}
