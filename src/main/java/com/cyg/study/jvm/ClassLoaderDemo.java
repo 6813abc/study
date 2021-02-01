4552527c549433bef16046d11169e3cf4d262b1c
chenyg5 <chenyg5@asiainfo.com>
2021-02-01 11:19:58 +0800

    类加载器

--- src/main/java/com/cyg/study/jvm/ClassLoaderDemo.java
+++ src/main/java/com/cyg/study/jvm/ClassLoaderDemo.java
@@ -4,7 +4,7 @@ package com.cyg.study.jvm;
  * @author cyg
  * @date 2021/2/1 11:12
  **/
-public class ClassLoaderDemo {
+public class ClassLoaderDemo extends ClassLoader{
     /**
      * 每个类加载都有一个父类加载器，我们通过下面的程序来验证。
      **/
@@ -12,5 +12,10 @@ public class ClassLoaderDemo {
         System.out.println("ClassLodarDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader());
         System.out.println("The Parent of ClassLodarDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader().getParent());
         System.out.println("The GrandParent of ClassLodarDemo's ClassLoader is " + ClassLoaderDemo.class.getClassLoader().getParent().getParent());
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        return super.findClass(name);
     }
 }
