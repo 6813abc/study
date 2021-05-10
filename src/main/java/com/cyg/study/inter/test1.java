ebc2f724718b373f1059a0c2dc175dd2a4637700
chenyg5 <chenyg5@asiainfo.com>
2021-05-10 09:52:56 +0800

    test

---
+++ src/main/java/com/cyg/study/inter/test1.java
@@ -0,0 +1,14 @@
+package com.cyg.study.inter;
+
+/**
+ * @author cyg
+ * @date 2021/5/8 9:35
+ **/
+public interface test1 {
+
+    int b = 5;
+
+    void test1();
+
+    void test2();
+}
---
+++ src/main/java/com/cyg/study/inter/testClass.java
@@ -0,0 +1,10 @@
+package com.cyg.study.inter;
+
+/**
+ * @author cyg
+ * @date 2021/5/8 9:37
+ **/
+public abstract class testClass implements test,test1 {
+
+
+}
