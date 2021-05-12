bfed0243ffc72407003b5925ca4d0ed4070e2efb
chenyg5 <chenyg5@asiainfo.com>
2021-05-12 15:33:01 +0800

    模拟多线程hashmap异常

--- src/main/java/com/cyg/study/concurrent/map/MapTest.java
+++ src/main/java/com/cyg/study/concurrent/map/MapTest.java
@@ -1,18 +1,27 @@
 package com.cyg.study.concurrent.map;
 
-import java.util.*;
+import java.util.HashMap;
 
 /**
+ * jdk1.6复现
+ *
  * @author cyg
  * @date 2021/5/10 9:45
  **/
 public class MapTest {
     public static void main(String[] args) {
-
-        Map hashMap = new HashMap();
-        hashMap = Collections.synchronizedMap(hashMap);
-
-        List list = new ArrayList();
-        list = Collections.singletonList(list);
+        HashMap hashMap = new HashMap(2, 0.75f);
+        hashMap.put(5, 55);
+        new Thread(() -> {
+            hashMap.put(7, 77);
+            System.out.println(hashMap);
+        }).start();
+        new Thread(() -> {
+            hashMap.put(3, 33);
+            System.out.println(hashMap);
+        }).start();
+        System.out.println(hashMap.get(5));
+        System.out.println(hashMap.get(7));
+        System.out.println(hashMap.get(3));
     }
 }
