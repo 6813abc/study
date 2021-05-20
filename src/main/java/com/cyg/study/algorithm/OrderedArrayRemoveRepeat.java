bcb1fbc9ea6c04b2b4fa8fc9037ef55ce4ce504b
chenyg5 <chenyg5@asiainfo.com>
2021-05-20 17:57:01 +0800

    在有序数组中去掉重复的数字，返回一共有多少个不重复的数字

---
+++ src/main/java/com/cyg/study/algorithm/OrderedArrayRemoveRepeat.java
@@ -0,0 +1,33 @@
+package com.cyg.study.algorithm;
+
+/**
+ * 在有序数组中去掉重复的数字，返回一共有多少个不重复的数字
+ *
+ * @author cyg
+ * @date 2021/5/20 17:17
+ **/
+public class OrderedArrayRemoveRepeat {
+
+    public static int orderedArrayRemoveRepeat(int[] arr) {
+        if (arr.length == 0 || arr.length == 1) {
+            return arr.length;
+        }
+        int m = 0;
+        int n = 1;
+        int len = arr.length;
+        while (n < arr.length) {
+            if (arr[m] == arr[n]) {
+                len--;
+            }
+            m++;
+            n++;
+        }
+        return len;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 1};
+        System.out.println(orderedArrayRemoveRepeat(arr));
+    }
+}
