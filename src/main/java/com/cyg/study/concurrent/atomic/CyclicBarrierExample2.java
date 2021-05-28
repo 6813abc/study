2e50dce98ecf18cb1c734b3fcd903ffe226e899a
chenyg5 <chenyg5@asiainfo.com>
2021-05-28 17:43:11 +0800

    同步器

---
+++ src/main/java/com/cyg/study/concurrent/atomic/CyclicBarrierExample2.java
@@ -0,0 +1,43 @@
+package com.cyg.study.concurrent.atomic;
+
+import java.util.concurrent.*;
+
+/**
+ * @author cyg
+ * @date 2021/5/26 17:49
+ **/
+public class CyclicBarrierExample2 {
+    // 请求的数量
+    private static final int threadCount = 550;
+    // 需要同步的线程数量
+    private static final CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+
+    public static void main(String[] args) throws InterruptedException {
+        // 创建线程池
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            threadPool.execute(() -> {
+                try {
+                    test(threadNum);
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        threadPool.shutdown();
+    }
+
+    public static void test(int threadnum) throws InterruptedException, BrokenBarrierException {
+        System.out.println("threadnum:" + threadnum + "is ready");
+        try {
+            cyclicBarrier.await();
+        } catch (Exception e) {
+            System.out.println("-----CyclicBarrierException------");
+        }
+        System.out.println("threadnum:" + threadnum + "is finish");
+    }
+
+}
