0110620aca735d1aceeb6400e5b677ed24b6ca8c
chenyg5 <chenyg5@asiainfo.com>
2021-06-15 10:36:07 +0800

    输入一个链表，输出一个链表，该输出链表包含原链表中从倒数第k个结点至尾节点的全部节点。  * 如果该链表长度小于k，请返回一个长度为 0 的链表。

---
+++ src/main/java/com/cyg/study/algorithm/swordFingerOffer/findKthToTail.java
@@ -0,0 +1,42 @@
+package com.cyg.study.algorithm.swordFingerOffer;
+
+/**
+ * 输入一个链表，输出一个链表，该输出链表包含原链表中从倒数第k个结点至尾节点的全部节点。
+ * 如果该链表长度小于k，请返回一个长度为 0 的链表。
+ *
+ * @author cyg
+ * @date 2021/6/15 10:08
+ **/
+public class findKthToTail {
+    public static class ListNode {
+        int val;
+        ListNode next = null;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode FindKthToTail(ListNode pHead, int k) {
+        if (pHead == null || k == 0) {
+            return null;
+        }
+        ListNode right = pHead;
+        while (k > 1) {
+            if (right.next != null) {
+                right = right.next;
+                k--;
+            } else {
+                return null;
+            }
+        }
+        while (true) {
+            if (right.next != null) {
+                right = right.next;
+                pHead = pHead.next;
+            } else {
+                return pHead;
+            }
+        }
+    }
+}
