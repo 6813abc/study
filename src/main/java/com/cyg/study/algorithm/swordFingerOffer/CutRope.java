f315e401a44716a9f031c09572995ae73319d024
chenyg5 <chenyg5@asiainfo.com>
2021-06-15 17:07:37 +0800

    给你一根长度为n的绳子，请把绳子剪成整数长的m段（m、n都是整数，n>1并且m>1，m<=n），  * 每段绳子的长度记为k[1],...,k[m]。请问k[1]x...xk[m]可能的最大乘积是多少？  * 例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。

---
+++ src/main/java/com/cyg/study/algorithm/swordFingerOffer/CutRope.java
@@ -0,0 +1,31 @@
+package com.cyg.study.algorithm.swordFingerOffer;
+
+/**
+ * 给你一根长度为n的绳子，请把绳子剪成整数长的m段（m、n都是整数，n>1并且m>1，m<=n），
+ * 每段绳子的长度记为k[1],...,k[m]。请问k[1]x...xk[m]可能的最大乘积是多少？
+ * 例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ *
+ * @author cyg
+ * @date 2021/6/15 16:18
+ **/
+public class CutRope {
+
+    public int cutRope(int target) {
+        if (target == 0) return 0;
+        if (target == 1 || target == 2) return 1;
+        if (target == 3) return 2;
+        int m = target % 3;
+        target = target / 3;
+        target = (int) Math.pow(3, target - 1);
+        switch (m) {
+            case 0:
+                return target * 3;
+            case 1:
+                return target * 4;
+            case 2:
+                return target * 6;
+            default:
+                return 0;
+        }
+    }
+}
